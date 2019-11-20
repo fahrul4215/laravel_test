c41b77ba5986def142cbabe0b621768a69d3fb25
Fahrul Yurisnan <fahrul4215@gmail.com>
2019-11-20 23:39:34 +0700

    socialite FB success

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('auth/{provider}', 'Auth\SocialiteController@redirectToProvider');
+Route::get('auth/{provider}/callback', 'Auth\SocialiteController@handleProviderCallback');
